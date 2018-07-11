30d3ca001364323b7f07134d533a7eeb3c0e3bd1
deks23 <deks232@gmail.com>
2018-07-11 21:27:27 +0200

    initial commit

--- src/main/java/pl/damiankotynia/app/controller/TestController.java
+++ src/main/java/pl/damiankotynia/app/controller/TestController.java
@@ -1,4 +1,14 @@
 package pl.damiankotynia.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    String qwe (){
+        return "qweqwe";
+    }
 }
