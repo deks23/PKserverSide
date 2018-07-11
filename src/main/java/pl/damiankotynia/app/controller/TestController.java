7a116844271a91102f0f59c0841f016c3bcae3bc
deks23 <deks232@gmail.com>
2018-07-11 21:48:41 +0200

    removed spring security

--- src/main/java/pl/damiankotynia/app/controller/TestController.java
+++ src/main/java/pl/damiankotynia/app/controller/TestController.java
@@ -3,8 +3,9 @@ package pl.damiankotynia.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController 
 public class TestController {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
